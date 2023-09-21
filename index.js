344253daf46232ec3e94253f99f844151b7c7dc2
Naeem-ur-rahman <naeemur1001@gmail.com>
2023-09-21 23:40:50 +0500

    Authentication: JWT, Services, Middlewares, conditional renders

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const path = require('path')
 const mongoose = require("mongoose")
+const cookieParser = require('cookie-parser');
 
-const userRoute = require('./routes/user')
+const userRoute = require('./routes/user');
+const { checkForAuthenticationCookie } = require('./middlewares/authentication');
 
 const app = express();
 const PORT = 3000;
@@ -15,8 +17,15 @@ mongoose
 app.set('view engine', "ejs")
 app.set("views", path.resolve('./views'))
 app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie('token'));
 
-app.get('/', (req, res) => res.render('home'))
+app.get('/', (req, res) => {
+     res.render('home', {
+          user: req.user,
+     })
+});
+
 app.use('/user', userRoute);
 
 app.listen(PORT, () => console.log(`Server Started at PORT ${PORT}`));
