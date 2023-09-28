18f134a37f47e309428559fd8f057ae11f4acca3
Naeem-ur-rahman <naeemur1001@gmail.com>
2023-09-29 00:52:22 +0500

    Comments Model + full functional Addition

--- routes/blog.js
+++ routes/blog.js
@@ -2,7 +2,8 @@ const { Router } = require('express');
 const multer = require('multer');
 const path = require('path');
 
-const Blog = require('../models/blog')
+const Blog = require('../models/blog');
+const Comment = require('../models/comment');
 
 const router = Router();
 
@@ -37,10 +38,21 @@ router.post('/', upload.single('coverImage'), async (req, res) => {
 
 router.get('/:id', async (req, res) => {
      const blog = await Blog.findById(req.params.id).populate('createdBy')
+     const comments = await Comment.find({ blogId: req.params.id }).populate('createdBy')
      return res.render('blog', {
           blog,
           user: req.user,
+          comments,
      })
 })
+
+router.post('/comment/:blogId', async (req, res) => {
+     await Comment.create({
+          content: req.body.content,
+          blogId: req.params.blogId,
+          createdBy: req.user._id,
+     })
+     res.redirect(`/blog/${req.params.blogId}`);
+});
 
 module.exports = router;
